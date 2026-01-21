768ad2cec55e1caad5637e706d0fd897d52715f1
SarthakStha <sarthak3926@gmail.com>
2026-01-21 17:10:39 -0600

    Added sidebar component from shadCN UI

--- nav-app/src/app/home/page.tsx
+++ nav-app/src/app/home/page.tsx
@@ -6,17 +6,17 @@ export default async function HomePage() {
   await requireAuth();
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-3xl font-bold text-[#00853E]">Welcome to UNT Navigator</h1>
-      <p className="mb-4">Select a route or explore the campus map.</p>
+      {/* <h1 className="text-3xl font-bold text-[#00853E]">Welcome to UNT Navigator</h1>
+      <p className="mb-4">Select a route or explore the campus map.</p> */}
       {/* Map at the top */}
-      <div className="w-full flex justify-center">
+      <div className="w-full h-full flex justify-center">
         <UNTLiveMap />
       </div>
 
       {/* Events widget at the bottom */}
-      <div className="w-full max-w-5xl">
+      {/* <div className="w-full max-w-5xl">
         <UNTEventsWidget />
-      </div>
+      </div> */}
     </div>
   );
 }
