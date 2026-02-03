7ae2dfb33a6b30d2a831c8f6f3e460ac9873bfdc
umanggpatel <patelumang918@gmail.com>
2026-02-03 12:10:28 -0600

    Trying to fix and test the invalid-origin error on Vercel

--- nav-app/src/lib/auth.ts
+++ nav-app/src/lib/auth.ts
@@ -2,12 +2,21 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 
+const VERCEL_URL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : undefined;
+
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", 
-    }),
-    emailAndPassword: {
-      enabled: true,
-      autoSignIn: true,
-    }
+  baseURL: VERCEL_URL,
+
+  trustedOrigins: VERCEL_URL ? [VERCEL_URL] : [],
+
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: true,
+  },
 });
