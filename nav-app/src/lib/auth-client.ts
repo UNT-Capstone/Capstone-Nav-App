f7c10383ae3e115d6800eb21767aee7feeccfbae
umanggpatel <patelumang918@gmail.com>
2026-02-03 12:44:28 -0600

    Againn trying to fix and test the invalid-origin error on Vercel

--- nav-app/src/lib/auth-client.ts
+++ nav-app/src/lib/auth-client.ts
@@ -1,10 +1,3 @@
 import { createAuthClient } from "better-auth/client";
 
-const BASE_URL =
-  typeof window === "undefined"
-    ? undefined
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
-
-export const authClient = createAuthClient({
-  baseURL: BASE_URL,
-});
+export const authClient = createAuthClient();
