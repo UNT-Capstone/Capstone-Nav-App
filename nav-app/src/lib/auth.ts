d7311899717f6e68eec985363dce596a6206384d
Nikhil Karki <nkarki2019@gmail.com>
2026-03-03 07:49:05 -0600

    fix: add baseURL to auth for vercel deployment

--- nav-app/src/lib/auth.ts
+++ nav-app/src/lib/auth.ts
@@ -6,6 +6,8 @@ export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", 
     }),
+    
+    baseURL: process.env.BETTER_AUTH_URL || `https://${process.env.VERCEL_URL}`,
     emailAndPassword: {
       enabled: true,
       autoSignIn: true,
