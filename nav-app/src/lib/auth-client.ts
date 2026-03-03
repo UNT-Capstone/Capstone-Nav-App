f632c58e20f82ea441cebd2e620ff25b44212a04
Nikhil Karki <nkarki2019@gmail.com>
2026-03-03 07:43:07 -0600

    feat: handle async searchParams and prepare map for navigation props

--- nav-app/src/lib/auth-client.ts
+++ nav-app/src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/client";
 
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL: "http://localhost:3000"
-    baseURL: "http://localhost:3000"
+  /** The base URL of the server (optional if you're using the same domain) */
+  //baseURL: "http://localhost:3000"
+  baseURL: "https://unt-nav-app.vercel.app/"
 });
