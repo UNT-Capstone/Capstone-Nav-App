2a48e47c3a8ad907e7e657eca3fb9097c8aa5b33
Nikhil Karki <69348104+gniman@users.noreply.github.com>
2026-04-13 02:27:40 -0500

    Update authClient baseURL to new deployment

--- nav-app/src/lib/auth-client.ts
+++ nav-app/src/lib/auth-client.ts
@@ -1,8 +1,4 @@
 import { createAuthClient } from "better-auth/client";
-
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  //baseURL: "http://localhost:3000"
-  baseURL: "https://capstone-wheat-eight.vercel.app"
+  baseURL: "https://capstone-lp3fbi5o9-gnimans-projects.vercel.app"
 });
-
