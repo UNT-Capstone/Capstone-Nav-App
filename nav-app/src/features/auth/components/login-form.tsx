afdbbbcead01910e13ccc2bfaa6171b32435c8b7
Nikhil Karki <69348104+gniman@users.noreply.github.com>
2026-04-13 03:16:01 -0500

    Update login-form.tsx

--- nav-app/src/features/auth/components/login-form.tsx
+++ nav-app/src/features/auth/components/login-form.tsx
@@ -63,7 +63,7 @@ export default function LoginForm() {
     try {
       await authClient.requestPasswordReset({
         email: forgotEmail,
-        redirectTo: ""https://capstone-bice-psi.vercel.app/reset-password",
+        redirectTo: "https://capstone-bice-psi.vercel.app/reset-password",
       });
       setForgotStatus("sent");
     } catch (err: any) {
